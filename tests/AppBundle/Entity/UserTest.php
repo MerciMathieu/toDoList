4870689271c2059ca82099a0859d6dfa7683c8c7
mathieu.delclos <mathieu.delclos@internethic.com>
2021-05-21 19:02:28 +0200

    Test tasks collection + test add and remove roles

--- tests/AppBundle/Entity/UserTest.php
+++ tests/AppBundle/Entity/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\AppBundle\Entity;
 
+use App\Entity\Task;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
@@ -11,5 +12,54 @@ class UserTest extends TestCase
     {
         $user = new User();
         $this->assertEquals(['ROLE_USER'], $user->getRoles());
+    }
+
+    public function testUserGetTasks()
+    {
+        $user = new User();
+        $user->setUsername('test');
+        $user->setPassword('test');
+        $user->setEmail('test@test.fr');
+
+        $task = new Task();
+        $task->setTitle('test');
+        $task->setContent('test');
+        $task->setAuthor($user);
+
+        $user->addTask($task);
+
+        $this->assertEquals(1, $user->getTasks()->count());
+    }
+
+    public function testUserRemoveTask()
+    {
+        $user = new User();
+        $user->setUsername('test');
+        $user->setPassword('test');
+        $user->setEmail('test@test.fr');
+
+        $task = new Task();
+        $task->setTitle('test');
+        $task->setContent('test');
+        $task->setAuthor($user);
+
+        $user->addTask($task);
+
+        $this->assertEquals(1, $user->getTasks()->count());
+
+        $user->removeTask($task);
+
+        $this->assertEquals(0, $user->getTasks()->count());
+    }
+
+    public function testAddUserRole()
+    {
+        $user = new User();
+        $user->setUsername('test');
+        $user->setPassword('test');
+        $user->setEmail('test@test.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $this->assertContains('ROLE_ADMIN', $user->getRoles());
     }
 }
