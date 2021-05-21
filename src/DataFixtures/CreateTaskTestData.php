2eb7c9124f8cfb54177b8314367e6231b473c351
mathieu.delclos <mathieu.delclos@internethic.com>
2021-05-21 23:43:16 +0200

    add user2 in fixtures + task with anonymous user + tests restrictions from voter

--- src/DataFixtures/CreateTaskTestData.php
+++ src/DataFixtures/CreateTaskTestData.php
@@ -15,13 +15,20 @@ class CreateTaskTestData extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $taskAnonymousAuthor = new Task();
+        $taskAnonymousAuthor->setTitle('author anonymous');
+        $taskAnonymousAuthor->setContent('author anonymous content test');
+
         $task = new Task();
         $task->setTitle('test');
         $task->setContent('test');
 
         $user = $this->getReference(CreateUserTestData::TEST_USER_REFERENCE);
+
         /** @var User $user */
         $task->setAuthor($user);
+
+        $manager->persist($taskAnonymousAuthor);
         $manager->persist($task);
 
         $manager->flush();
